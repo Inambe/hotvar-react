43b2c888c85f8df3f88003312d01fa468216a16d
Inam Ul Huq <inambe.dev@gmail.com>
2024-09-04 00:01:42 +0500

    introduce `useHotVar`

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useHotVar } from "./main";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { values } = useHotVar({
+    vars: {
+      "inambe:seats-left": {
+        type: "number",
+        placeholder: 0,
+      },
+    },
+  });
 
+  console.log(values);
   return (
     <>
       <div>
@@ -29,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "HotVar",
+      // the proper extensions will be added
+      fileName: "hotvar",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+});
